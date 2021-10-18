aa1abbb8da588aeff00f2ff35ac09239877f284c
Frans Johansson <fransjohansson98@gmail.com>
2021-10-18 11:44:50 +0200

    Implement complete sketch sampling and improve API

--- utils/sampling.py
+++ utils/sampling.py
@@ -35,7 +35,7 @@ def normalized_gmm_params(params):
     return pi, mu_x, mu_y, sigma_x, sigma_y, rho_xy
 
 
-def sample_stroke_offset(mu, sigma, rho):
+def _sample_stroke_offset(mu, sigma, rho):
     """
     Sample the stroke offsets dx and dy from a multivariate normal distribution.
 
@@ -50,7 +50,7 @@ def sample_stroke_offset(mu, sigma, rho):
     return torch.from_numpy(s)
 
 
-def sample_pen_state(q):
+def _sample_pen_state(q):
     """
     Samples the one-hot encoded pen state for the stroke-5 representation of pen strokes
 
@@ -61,25 +61,33 @@ def sample_pen_state(q):
     return ohc.sample().to(device)
 
 
-def sample_stroke_gmm(weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q):
+def sample_stroke(weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q):
     """
-    Samples a stroke offset dx and dy from a GMM with M mixture components.
+    Samples a single stroke offset dx and dy from a GMM with M mixture components, and
+    a one-hot encoded pen state from a categorical distribution of three possible values. 
 
     Args:
-        TODO: Add args
+        weights: An M dimensional tensor of mixture weights
+        mu_x, mu_y, sigma_x, sigma_y, rho_xy: Each an M dimensional tensor with parameters for
+            each of the mixture components
+        q: A 3 dimensional tensor of categorical probabilities for the pen states
+
+    Returns:
+        A 5 dimensional tensor containing [dx, dy, p1, p2, p3], with dx and dy being stroke offsets
+        and each p representing a different pen state (down, up and end-of-sequence).
     """
     # Select one of the mixtures by their weights
     c = torch.distributions.Categorical(probs=weights)
     m = c.sample()
 
     # Handle the sampling
-    mu = torch.cat([mu_x[m], mu_y[m]])
-    sigma = torch.cat([sigma_x[m], sigma_y[m]])
+    mu = torch.stack([mu_x[m], mu_y[m]])
+    sigma = torch.stack([sigma_x[m], sigma_y[m]])
     
-    stroke = sample_stroke_offset(
+    stroke = _sample_stroke_offset(
         mu, sigma, rho_xy[m]
     )
-    pen = sample_pen_state(q)
+    pen = _sample_pen_state(q)
 
     # Concatenate into one stroke-5 tensor
     return torch.cat([stroke, pen]).to(device)
@@ -90,10 +98,20 @@ def sample_sketch(params):
     Samples a complete sketch sequence from the parameters output by e.g. the SkethRNN model
     
     Args:
-        TODO: Add args
+        params: A tuple of (weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q), each except q being
+            a (batch, stroke, mixture) tensor with q being a (batch, stroke, 3) tensor
     """
-    strokes = [
-        sample_stroke_gmm(weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q)
-        for weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q in params
-    ]
+    num_batches = params[0].shape[0]
+    batches = []
+
+    for batch in range(num_batches):
+        batch_params = tuple([param[batch] for param in params])
+        strokes = [
+            sample_stroke(weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q)
+            for weights, mu_x, mu_y, sigma_x, sigma_y, rho_xy, q in zip(*batch_params)
+        ]
+
+        batches.append(torch.stack(strokes))
+    
+    return torch.stack(batches)
 
